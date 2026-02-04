cf47ce445e2b1dae4501912a0fc2b997c72021ed
ToonRz <xkingxzelz@gmail.com>
2026-02-05 00:35:02 +0700

    update

--- MarketWiseProject/app/build.gradle.kts
+++ MarketWiseProject/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.marketwiseproject"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.marketwiseproject"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
